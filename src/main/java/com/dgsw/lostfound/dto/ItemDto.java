7ed8820ea61377e495ae5d62dd5fbef31ff9a0a7
bong <bongchok45@gmail.com>
2026-06-23 12:38:15 +0900

    분실물 API 기본 구조 생성

---
+++ src/main/java/com/dgsw/lostfound/dto/ItemDto.java
@@ -0,0 +1,14 @@
+package com.dgsw.lostfound.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDto {
+
+    private Long id;
+    private String itemName;
+
+}
